7670b24e01cff28a92a94bdc5ac80e1f2bd2495d
anilmicroservices <anilreddy2011@outlook.com>
2023-07-05 17:02:23 +0530

    Order CheckOut, Update and Delete Commands

--- Services/Catalog/ConsoleApp1/ConsoleApp1/Program.cs
+++ Services/Catalog/ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,47 +2,20 @@
 
 class AnagramChecker
 {
-    static void Main(string[] args)
+    public static void Main()
     {
-        Console.Write("Enter the first word: ");
-        string word1 = Console.ReadLine();
-
-        Console.Write("Enter the second word: ");
-        string word2 = Console.ReadLine();
-
-        if (IsAnagram(word1, word2))
-        {
-            Console.WriteLine("{0} and {1} are anagrams.", word1, word2);
-        }
-        else
-        {
-            Console.WriteLine("{0} and {1} are not anagrams.", word1, word2);
-        }
-
+        List<test> list = new List<test> {
+            new test { Id= 8, name = "A" }, 
+            new test { Id= 2, name= "B" },
+            new test { Id= 4, name= "B" },
+        };
+        int res = list.Where(m=>m.name=="B").Sum(s => s.Id);
+        Console.WriteLine("Sum = {0}", res);
         Console.ReadLine();
     }
-
-    static bool IsAnagram(string word1, string word2)
-    {
-        if (word1.Length != word2.Length)
-        {
-            return false;
-        }
-
-        char[] chars1 = word1.ToLower().ToCharArray();
-        char[] chars2 = word2.ToLower().ToCharArray();
-
-        //Array.Sort(chars1);
-        //Array.Sort(chars2);
-
-        for (int i = 0; i < chars1.Length; i++)
-        {
-            if (chars1[i] == chars2[i])
-            {
-                return false;
-            }
-        }
-
-        return true;
-    }
+}
+public class test
+{
+    public int Id;
+    public string name;
 }
